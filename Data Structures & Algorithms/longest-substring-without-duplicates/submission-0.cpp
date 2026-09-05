ce6947c1b74360e3750e803380290d3008101521
CainMacias <cain.maciasb@gmail.com>
2026-09-05 12:13:24 -0600

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+    unordered_set<char> window;
+    int l = 0, max_len = 0;
+    for (int r = 0; r < s.length(); r++) {
+        while (window.count(s[r])) {
+            window.erase(s[l]);
+            l++;
+        }
+        window.insert(s[r]);
+        max_len = max(max_len, r - l + 1);
+        }
+
+        return max_len;
+    }
+};
